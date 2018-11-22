1e233fa73bcfa1b4a762c1b9d87749c37f14a072
llluper <mitchelljstanford@gmail.com>
2018-11-22 12:08:01 +1100

    Selector comments

--- src/containers/CardsContainer/selectors.js
+++ src/containers/CardsContainer/selectors.js
@@ -1,18 +1,18 @@
 import { createSelector } from 'reselect'
 
-// select cards from state
+// select cards list from state
 export const cardsSelector = createSelector(
   state => state.cards.list,
   items => items
 )
 
-// select cards from state
+// select cards pages from state
 export const pagesSelector = createSelector(
   state => state.cards.pages,
   items => items
 )
 
-// select cards from state
+// select cards details from state
 export const detailsSelector = createSelector(
   state => state.cards.details,
   items => items
